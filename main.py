3b195d30682d1b499bfea84da603662fc5330e64
Niels <nielsz@pm.me>
2024-04-21 21:58:30 +0200

    Add authorization check for bot commands and message handling

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+AUTHORIZED_USERS = [5964123828]
+
 import telebot
 import os
 import time
@@ -7,14 +9,21 @@ load_dotenv()
 
 bot = telebot.TeleBot(os.getenv('TELEGRAM_BOT_TOKEN'))
 location = os.getenv('DOWNLOAD_LOCATION')
-
+def is_authorized(message):
+    return message.from_user.id in AUTHORIZED_USERS
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    if not is_authorized(message):
+        bot.reply_to(message, "You are not authorized to use this bot.")
+        return
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(func=lambda message: message.text.startswith('https://open.spotify.com/track/'))
 def echo_all(message):
+    if not is_authorized(message):
+        bot.reply_to(message, "You are not authorized to use this bot.")
+        return
     download_process = subprocess.Popen(['python3', '-m', 'spotdl', message.text], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=location)
     bot.reply_to(message, "Downloading...")
     for line in download_process.stdout:
